a4fee84533ab2ae32e5f74b1e6d0ba20eca828e7
slipiduche <alejandrocamacaro91@gmail.com>
2020-12-17 09:51:47 -0400

    delete tags

---
+++ lib/src/pages/deleteTags_page.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:systemAPP/constants.dart';
+import 'package:systemAPP/src/bloc/serverData_bloc.dart';
+import 'package:systemAPP/src/icons/icons.dart';
+import 'package:systemAPP/src/widgets/widgets.dart';
+
+class DeleteTagsPage extends StatefulWidget {
+  DeleteTagsPage({Key key}) : super(key: key);
+
+  @override
+  _DeleteTagsPageState createState() => _DeleteTagsPageState();
+}
+
+class _DeleteTagsPageState extends State<DeleteTagsPage> {
+  bool tagHere=false,songHere=false;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          color: colorBackGround,
+          child: Column(
+            children: [
+              Container(
+                  height: 10.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: gradiente,
+                  )),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 26.0),
+                      Container(
+                        height: 123,
+                        width: 123,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(100.0)),
+                        child: tagIcon(98.0, colorMedico),
+                      ),
+                      SizedBox(height: 8.0),
+                      Text(
+                        'Tags',
+                        style: TextStyle(
+                            color: colorVN,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      //Scan the tag  you want to Delete using your register device
+                      SizedBox(height: 15.0),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Scan the tag  you want to delete using your register device',
+                          style: TextStyle(
+                            fontSize: 24.0,
+                          ),
+                          overflow: TextOverflow.clip,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: 25.0),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 25.0),
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Tag code',
+                              style: TextStyle(
+                                fontSize: 25.0,
+                              ),
+                              // overflow: TextOverflow.clip,
+                              // textAlign: TextAlign.left,
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            StreamBuilder(
+                              stream: ServerDataBloc().tagStream,
+                              builder: (BuildContext context,
+                                  AsyncSnapshot snapshot) {
+                                if (snapshot.hasData) {
+                                  tagHere=true;
+                                  return textBoxForm(snapshot.data, context);
+                                } else {
+                                  return textBoxForm(
+                                      'The tag will appear here', context);
+                                }
+                              },
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              'Bind a song',
+                              style: TextStyle(
+                                fontSize: 25.0,
+                              ),
+                              // overflow: TextOverflow.clip,
+                              // textAlign: TextAlign.left,
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            GestureDetector(
+                              onTap: tagHere?(){
+                                //Navigator.of(context).pushNamed('bindSong');
+                                print('nothing to do');
+                              }:null,
+                              child: StreamBuilder(
+                                stream: ServerDataBloc().tagStream,
+                                builder: (BuildContext context,
+                                    AsyncSnapshot snapshot) {
+                                  if (snapshot.hasData) {
+                                    songHere=true;
+                                    return searchBoxForm(
+                                        snapshot.data, context);
+                                  } else {
+                                    return searchBoxForm(
+                                        'Select a song from the list', context);
+                                  }
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Center(child: submitButton('Done', tagHere?(songHere?(){
+                              print('send new tag');
+                            }:(){}):(){}),)
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              gradientBar2(1),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomBar(1),
+      ),
+    );
+  }
+}
